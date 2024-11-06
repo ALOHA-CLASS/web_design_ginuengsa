a083969ccc0de2bfa4c8f22e44d6a47853eaf830
wwwalohacampus <h850415@naver.com>
2024-11-06 21:16:41 +0900

    샘플 준비

--- 연습/A-1/script/script.js
+++ 연습/A-1/script/script.js
@@ -2,33 +2,32 @@
 $(function() {
     // 메뉴 슬라이드
     $(".main-menu").on("mouseover", function(){
-        $(".sub-menu").stop().fadeIn()
+        // $(".sub-menu").stop().fadeIn()
+        $(".sub-menu").stop().slideDown()
     })
     $(".main-menu").on("mouseout", function(){
-        $(".sub-menu").stop().fadeOut()
+        // $(".sub-menu").stop().fadeOut()
+        $(".sub-menu").stop().slideUp()
     })
 
     // 이미지 슬라이드 - 세로
-    /*
-    let index = 0;
-    const speed = 500;
-    const time = 3000;
-    const $slide = $(".slide")
-    const $container = $(".slide-container")
-    const size = $slide.height();
-    const count = $slide.length;
-    $container.height(size * count)
+    // let index = 0;
+    // const speed = 500;
+    // const time = 3000;
+    // const $slide = $(".slide")
+    // const $container = $(".slide-container")
+    // const size = $slide.height();
+    // const count = $slide.length;
+    // $container.height(size * count)
     
-    setInterval(function() {
-        index = (index + 1) % count;
-        $container.animate({
-            top: -index * size
-        }, speed);
-    }, time);
-    */
+    // setInterval(function() {
+    //     index = (index + 1) % count;
+    //     $container.animate({
+    //         top: -index * size
+    //     }, speed);
+    // }, time);
 
     // 이미지 슬라이드 - 가로
-    /*
     let index = 0;
     const speed = 500;
     const time = 3000;
@@ -44,22 +43,22 @@ $(function() {
             left: -index * size
         }, speed);
     }, time);
-    */
     
 
     // 페이드 슬라이드
-    let index = 0;
-    const speed = 500;
-    const time = 3000;
-    const $slide = $(".slide")
-    const count = $(".slide").length;
-    $slide.not(":first").hide()
+    // let index = 0;
+    // const speed = 500;
+    // const time = 3000;
+    // const $slide = $(".slide")
+    // const count = $(".slide").length;
+    // $slide.not(":first").hide()
     
-    setInterval(function() {
-        index = (index + 1) % count;
-        $slide.fadeOut(speed)
-        $slide.eq(index).fadeIn(speed)
-    }, time);
+    // setInterval(function() {
+    //     index = (index + 1) % count;
+    //     $slide.fadeOut(speed)
+    //     $slide.eq(index).fadeIn(speed)
+    //     $slide.style("z-index", "-1")
+    // }, time);
     
 
     // 탭 기능
@@ -75,5 +74,14 @@ $(function() {
         $(".menu-item").removeClass("active")
         $(this).parent().addClass("active")
     })
+
+    // 레이어 팝업
+    $("#btn-modal").on("click", function() {
+        $("#modal").show();
+    });
+
+    $(".close").on("click", function() {
+        $("#modal").hide();
+    });
 
 })
