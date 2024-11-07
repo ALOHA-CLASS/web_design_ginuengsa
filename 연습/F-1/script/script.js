01167b3daa54f50a461be0f9d4b42add9fa67a90
wwwalohacampus <h850415@naver.com>
2024-11-08 08:27:30 +0900

    💚F-1

--- 연습/F-1/script/script.js
+++ 연습/F-1/script/script.js
@@ -1,14 +1,15 @@
 
 $(function() {
     // 메뉴 슬라이드
-    $(".main, .sub").on("mouseenter", function(){
-        // $(".sub-menu").stop().fadeIn()
-        $(".sub").stop().slideDown()
+    $(".main-menu li").on("mouseover", function(){
+        // $(this).find(".sub-menu").stop().fadeIn()
+        $(this).find(".sub-menu").stop().slideDown()
     })
-    $(".main, .sub").on("mouseleave", function(){
-        // $(".sub-menu").stop().fadeOut()
-        $(".sub").stop().slideUp()
+    $(".main-menu li").on("mouseout", function(){
+        // $(this).find(".sub-menu").stop().fadeOut()
+        $(this).find(".sub-menu").stop().slideUp()
     })
+
     // 이미지 슬라이드 - 세로
     // let index = 0;
     // const speed = 500;
@@ -61,18 +62,18 @@ $(function() {
     
 
     // 탭 기능
-    // $("#btn-notice").on("click", function() {
-    //     $("#content-notice").show()
-    //     $("#content-gallery").hide()
-    //     $(".menu-item").removeClass("active")
-    //     $(this).parent().addClass("active")
-    // })
-    // $("#btn-gallery").on("click", function() {
-    //     $("#content-notice").hide()
-    //     $("#content-gallery").show()
-    //     $(".menu-item").removeClass("active")
-    //     $(this).parent().addClass("active")
-    // })
+    $("#btn-notice").on("click", function() {
+        $("#content-notice").show()
+        $("#content-gallery").hide()
+        $(".menu-item").removeClass("active")
+        $(this).parent().addClass("active")
+    })
+    $("#btn-gallery").on("click", function() {
+        $("#content-notice").hide()
+        $("#content-gallery").show()
+        $(".menu-item").removeClass("active")
+        $(this).parent().addClass("active")
+    })
 
     // 레이어 팝업
     $(".btn-modal").on("click", function() {
