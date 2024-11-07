a8045f6d6d49189d62eddddf784c6e1a529ad6f2
wwwalohacampus <h850415@naver.com>
2024-11-08 00:10:22 +0900

    💛A-4

---
+++ 연습/A-4/script/script.js
@@ -0,0 +1,87 @@
+
+$(function() {
+    // 메뉴 슬라이드
+    $(".main, .sub").on("mouseenter", function(){
+        // $(".sub-menu").stop().fadeIn()
+        $(".sub").stop().slideDown()
+    })
+    $(".main, .sub").on("mouseleave", function(){
+        // $(".sub-menu").stop().fadeOut()
+        $(".sub").stop().slideUp()
+    })
+    
+    // 이미지 슬라이드 - 세로
+    // let index = 0;
+    // const speed = 500;
+    // const time = 3000;
+    // const $slide = $(".slide")
+    // const $container = $(".slide-container")
+    // const size = $slide.height();
+    // const count = $slide.length;
+    // $container.height(size * count)
+    
+    // setInterval(function() {
+    //     index = (index + 1) % count;
+    //     $container.animate({
+    //         top: -index * size
+    //     }, speed);
+    // }, time);
+
+    // 이미지 슬라이드 - 가로
+    // let index = 0;
+    // const speed = 500;
+    // const time = 3000;
+    // const $slide = $(".slide")
+    // const $container = $(".slide-container")
+    // const size = $slide.width();
+    // const count = $slide.length;
+    // $container.width(size * count)
+    
+    // setInterval(function() {
+    //     index = (index + 1) % count;
+    //     $container.animate({
+    //         left: -index * size
+    //     }, speed);
+    // }, time);
+    
+
+    // 페이드 슬라이드
+    let index = 0;
+    const speed = 500;
+    const time = 3000;
+    const $slide = $(".slide")
+    const count = $(".slide").length;
+    $slide.not(":first").hide()
+    
+    setInterval(function() {
+        index = (index + 1) % count;
+        $slide.fadeOut(speed)
+        $slide.eq(index).fadeIn(speed)
+        $slide.style("z-index", "-1")
+    }, time);
+    
+
+    // 탭 기능
+    $("#btn-notice").on("click", function() {
+        $("#content-notice").show()
+        $("#content-gallery").hide()
+        $(".menu-item").removeClass("active")
+        $(this).parent().addClass("active")
+    })
+    $("#btn-gallery").on("click", function() {
+        $("#content-notice").hide()
+        $("#content-gallery").show()
+        $(".menu-item").removeClass("active")
+        $(this).parent().addClass("active")
+    })
+
+    // 레이어 팝업
+    $(".btn-modal").on("click", function() {
+        $("#modal").show();
+    });
+
+    $(".close").on("click", function() {
+        $("#modal").hide();
+    });
+
+})
