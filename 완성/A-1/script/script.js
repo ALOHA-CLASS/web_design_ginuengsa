735c4fa122ec8a1865954986e08eaabd056b169a
wwwalohacampus <h850415@naver.com>
2024-11-11 18:52:25 +0900

    🕵️‍♂️ 따라치기

--- 완성/A-1/script/script.js
+++ 완성/A-1/script/script.js
@@ -1,6 +1,6 @@
-
 $(function() {
-    // 메뉴 슬라이드
+    /* ------------------- 💛메뉴 슬라이드💛 ------------------- */
+    /* ------------------- ↔ 가로 멀티        ------------------- */
     $(".main-menu").on("mouseover", function(){
         // $(".sub-menu").stop().fadeIn()
         $(".sub-menu").stop().slideDown()
@@ -9,47 +9,8 @@ $(function() {
         // $(".sub-menu").stop().fadeOut()
         $(".sub-menu").stop().slideUp()
     })
-
-    /* ------------------- 무한 반복 ❌ ------------------- */
-    // 이미지 슬라이드 - 세로
-    // let index = 0;
-    // const speed = 500;
-    // const time = 3000;
-    // const $slide = $(".slide")
-    // const $container = $(".slide-container")
-    // const size = $slide.height();
-    // const count = $slide.length;
-    // $container.height(size * count)
-    
-    // setInterval(function() {
-    //     index = (index + 1) % count;
-    //     $container.animate({
-    //         top: -index * size
-    //     }, speed);
-    // }, time);
-
-    // 이미지 슬라이드 - 가로
-    // let index = 0;
-    // const speed = 500;
-    // const time = 3000;
-    // const $slide = $(".slide")
-    // const $container = $(".slide-container")
-    // const size = $slide.width();
-    // const count = $slide.length;
-    // $container.width(size * count)
-    
-    // setInterval(function() {
-    //     index = (index + 1) % count;
-    //     $container.animate({
-    //         left: -index * size
-    //     }, speed);
-    // }, time);
-
-        
-    /* ------------------- 무한 반복 💛 ------------------- */
-    
-    // 이미지 슬라이드 - 세로 (💛무한반복) 
-    // * 마지막 이미지가 슬라이드 되면 다시 첫 번째 이미지가 슬라이드 되는 방식
+    /* ------------------- 💛이미지 슬라이드💛 ------------------- */
+    /* ------------------- ↕ 세로 슬라이드      ------------------- */
     const speed = 500;
     const time = 3000;
     const $slide = $(".slide")
@@ -57,7 +18,6 @@ $(function() {
     const size = $slide.height();
     const count = $slide.length;
     $container.height(size * count)
-    
     setInterval(function() {
         $container.animate({
             top: -size
@@ -66,43 +26,7 @@ $(function() {
             $container.append($(".slide").first());
         });
     }, time);
-
-    // 이미지 슬라이드 - 가로 (💛무한반복) 
-    // * 마지막 이미지가 슬라이드 되면 다시 첫 번째 이미지가 슬라이드 되는 방식
-    // const speed = 500;
-    // const time = 3000;
-    // const $slide = $(".slide")
-    // const $container = $(".slide-container")
-    // const size = $slide.width();
-    // const count = $slide.length;
-    // $container.width(size * count)
-    
-    // setInterval(function() {
-    //     $container.animate({
-    //         left: -size
-    //     }, speed, function() {
-    //         $container.css('left','0')
-    //         $container.append($(".slide").first());
-    //     } );
-    // }, time);
-
-    // 페이드 슬라이드
-    // let index = 0;
-    // const speed = 500;
-    // const time = 3000;
-    // const $slide = $(".slide")
-    // const count = $(".slide").length;
-    // $slide.not(":first").hide()
-    
-    // setInterval(function() {
-    //     index = (index + 1) % count;
-    //     $slide.fadeOut(speed)
-    //     $slide.eq(index).fadeIn(speed)
-    //     $slide.style("z-index", "-1")
-    // }, time);
-    
-
-    // 탭 기능
+    /* ------------------- 💛 탭(공지+갤러리) 💛 ------------------- */
     $("#btn-notice").on("click", function() {
         $("#content-notice").show()
         $("#content-gallery").hide()
@@ -115,14 +39,11 @@ $(function() {
         $(".menu-item").removeClass("active")
         $(this).parent().addClass("active")
     })
-
-    // 레이어 팝업
+    /* ------------------- 💛 레이어 팝업 💛 ------------------- */
     $(".btn-modal").on("click", function() {
         $("#modal").show();
     });
-
     $(".close").on("click", function() {
         $("#modal").hide();
     });
-
 })
