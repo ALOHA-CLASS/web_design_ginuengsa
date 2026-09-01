6d7d6d102df3a9957b2256b6aa12d34446b59696
wwwalohacampus <h850415@naver.com>
2026-09-01 17:35:10 +0900

    feat: 쉬운완성 C유형

---
+++ 쉬운완성/C-4/script/script.js
@@ -0,0 +1,21 @@
+$(function() {
+  // 가로 기본메뉴
+  $(".main-menu li").on("mouseover", function() { $(this).find(".sub-menu").stop().slideDown(); });
+  $(".main-menu li").on("mouseout", function() { $(this).find(".sub-menu").stop().slideUp(); });
+
+  // 슬라이드(가로 무한반복)
+  const speed = 500, time = 2000;
+  const $slide = $(".slide");
+  const $container = $(".slide-container");
+  const size = $slide.width();
+  $container.width(size * $slide.length);
+  setInterval(function() {
+    $container.animate({ left: -size }, speed, function() {
+      $container.css("left", "0");
+      $container.append($(".slide").first());
+    });
+  }, time);
+
+  // 레이어 팝업
+  $(".btn-modal").on("click", function() { $("#modal").toggle(); });
+});
