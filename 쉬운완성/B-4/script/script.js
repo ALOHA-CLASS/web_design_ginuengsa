ce7642bec852db6d25d105f4f3caa4ec83bc66bb
wwwalohacampus <h850415@naver.com>
2026-09-01 15:45:19 +0900

    feat: 쉬운완성 B유형

---
+++ 쉬운완성/B-4/script/script.js
@@ -0,0 +1,21 @@
+$(function() {
+  // 가로 100% 멀티메뉴
+  $(".main, .sub").on("mouseenter", function() { $(".sub").stop().slideDown(); });
+  $(".main, .sub").on("mouseleave", function() { $(".sub").stop().slideUp(); });
+
+  // 슬라이드(가로 무한반복)
+  const speed = 500, time = 2000;
+  const $slide = $(".slide");
+  const $container = $(".slide-container");
+  const size = $slide.width();
+  $container.width(size * $slide.length);
+  setInterval(function() {
+    $container.animate({ left: -size }, speed, function() {
+      $container.css("left", "0");
+      $container.append($(".slide").first());
+    });
+  }, time);
+
+  // 레이어 팝업
+  $(".btn-modal").on("click", function() { $("#modal").toggle(); });
+});
